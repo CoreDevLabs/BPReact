3c0f81824377ecfef8d0ee8063982e95fbf1fead
malcolmlowery <coredevlabs@gmail.com>
2019-10-31 18:20:35 -0400

    blueprintJS-React-BP

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "../node_modules/normalize.css";
+import "../node_modules/@blueprintjs/core/lib/css/blueprint.css";
+
+// Imported Component
+import App from './components/App';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
